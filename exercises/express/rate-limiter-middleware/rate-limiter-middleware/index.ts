6cb9058b3cd7142108e8823333ad2243d5bd8e33
Andie Similon <andie.similon@ap.be>
2024-03-17 21:30:45 +0000

    Add all files again

---
+++ exercises/express/rate-limiter-middleware/rate-limiter-middleware/index.ts
@@ -0,0 +1,29 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+import path from "path";
+import { ejsUtility } from "./middleware/ejsUtility";
+
+dotenv.config();
+
+const app : Express = express();
+
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+
+app.set("port", process.env.PORT || 3000);
+
+app.use(ejsUtility);
+
+app.get("/", (req, res) => {
+    res.render("index", {
+        title: "Hello World",
+        message: "Hello World"
+    })
+});
+
+app.listen(app.get("port"), () => {
+    console.log("Server started on http://localhost:" + app.get('port'));
+});
